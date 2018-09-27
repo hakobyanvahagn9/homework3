807023e750762aff33cdf5a12f4dac2c6564c2c5
Vahagn Hakobyan <43023191+hakobyanvahagn9@users.noreply.github.com>
2018-09-27 22:10:16 +0400

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,55 @@
+const n= function (a,b,c){
+return a-b+c;
+};
+
+const f = fuction (){
+};
+
+const m =function(t,s){
+const l = t*s;
+};
+
+const Vahag = function (num1,num2){
+   if (num1 === num2)
+   {return 0};
+   if (num1 > num2)
+   {return 1} else (num1 < num2) 
+   {return -1};
+};
+
+
+const M = function(num1,num2){
+return num1*num2;
+};
+
+const D = function (num1,num2){
+return num1/num2;
+};
+
+
+const triangleArea = function (base, height){
+const T = D(M(base,height),2);
+return T;
+};
+
+const numLength = function (num){
+const c = num.toString 
+return c;
+};
+
+const S = function(bla,blabla,num){
+const c = bla + blabla
+if (c.length > num)
+{retur 1}else 
+{return -1};
+};
+
+const runStuff = function (num1,num2,bla){
+   if (bla = "ractangle")
+   {return nam1*num2};
+   if (bla = "triangle")
+   {return triangleArea(num1,num2)}else 
+   {return 1};
+};
+   
+
